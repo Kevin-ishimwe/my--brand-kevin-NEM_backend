123e729b6a6777b30dab7bdaf284ee551a453395
newby101 <ishimwekevin45@gmail.com>
2023-01-30 16:52:16 +0200

    detect open handles fix and re-configuting test files

--- src/server.js
+++ src/server.js
@@ -4,14 +4,14 @@ import dotenv from 'dotenv';
 //connecting to Mbd and lsiten to port through that
 dotenv.config();
 const hosted = process.env.DB_LINK;
-const server = app;
+
 mongoose.set('strictQuery', true);
 mongoose
   .connect(hosted, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     if (process.env.NODE_ENV !== 'test') {
       try {
-        server.listen(8000, () => {
+      app.listen(8000, () => {
           console.log('server running');
           console.log('connected to dbs');
         });
@@ -21,4 +21,5 @@ mongoose
       }
     }
   });
-export default server;
+  
+
