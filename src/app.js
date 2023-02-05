ae3f31e29cdd5dbd6fbf3aaeb9d14e0f81e17fe7
newby101 <ishimwekevin45@gmail.com>
2023-02-05 13:26:09 +0200

    changing back to token processing

--- src/app.js
+++ src/app.js
@@ -19,7 +19,10 @@ app.use(
 app.use(cookieParser())
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+   origin : true,
+  credentials: true, // <= Accept credentials (cookies) sent by the client
+}));
 app.use(MessageRoutes);
 app.use(BlogRoutes);
 app.use(userRoutes);
